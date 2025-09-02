2e6fb37e0945a2d217cee8c04624fd833f99857c
최현준 <osumaniaddict527@gmail.com>
2025-09-03 00:04:38 +0900

    btsk-11: 프록시 관련 에러 해결 및 홈 컨트롤러 수정 (#9)

--- src/main/java/kr/it/pullit/modules/home/web/HomeController.java
+++ src/main/java/kr/it/pullit/modules/home/web/HomeController.java
@@ -2,10 +2,11 @@ package kr.it.pullit.modules.home.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/** 홈 컨트롤러 - 헬스 체크용 */
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 
   @GetMapping("/")
--- src/main/java/kr/it/pullit/platform/web/WebCorsProps.java
+++ src/main/java/kr/it/pullit/platform/web/WebCorsProps.java
@@ -7,12 +7,12 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /*
- * 디폴트 값을 http://localhost:3000으로 설정. yml에서 값 지정하면 덮어씌워짐.
+ * 디폴트 값을 http://localhost:5173으로 설정. yml에서 값 지정하면 덮어씌워짐.
  */
 @Setter
 @Getter
 @ConfigurationProperties(prefix = "cors")
 public class WebCorsProps {
 
-  private List<String> allowedOrigins = new ArrayList<>(List.of("http://localhost:3000"));
+  private List<String> allowedOrigins = new ArrayList<>(List.of("http://localhost:5173"));
 }
