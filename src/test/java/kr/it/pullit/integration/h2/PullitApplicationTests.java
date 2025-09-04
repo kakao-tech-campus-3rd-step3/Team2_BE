5175b12fb3c71c885288f349b37942786fdb12ed
HyeonJun <osumaniaddict527@gmail.com>
2025-09-05 08:46:13 +0900

    Refactor : 머지컨플릭트 해결

---
+++ src/test/java/kr/it/pullit/integration/h2/PullitApplicationTests.java
@@ -0,0 +1,10 @@
+package kr.it.pullit.integration.h2;
+
+import org.junit.jupiter.api.Test;
+import kr.it.pullit.support.H2IntegrationTest;
+
+class PullitApplicationTests extends H2IntegrationTest {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ src/test/java/kr/it/pullit/support/H2IntegrationTest.java
@@ -0,0 +1,11 @@
+package kr.it.pullit.support;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Disabled
+@ActiveProfiles("h2")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class H2IntegrationTest {
+}
