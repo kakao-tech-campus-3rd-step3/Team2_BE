f44d1615aa0b039d745012b778f9b3cfef04361f
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2025-09-05 15:54:44 +0900

    btsk-13: 문제집 정보 가져오기 (#17)
    * feat: 문제집 getById 구현
    
    * refactor: QuestionRepository jpa 의존 수정
    
    * docs: 모호한 필드 주석 추가
    
    * fix: Date를 LocalDateTime 으로 변경
    
    * fix(QuestioinSetService): Transactional 추가
    
    * fix(QuestionSetDto): Date -> LocalDateTime (createTime)

--- src/main/java/kr/it/pullit/modules/questionset/service/QuestionSetService.java
+++ src/main/java/kr/it/pullit/modules/questionset/service/QuestionSetService.java
@@ -1,5 +1,29 @@
 package kr.it.pullit.modules.questionset.service;
 
 import kr.it.pullit.modules.questionset.api.QuestionSetPublicApi;
+import kr.it.pullit.modules.questionset.domain.entity.QuestionSet;
+import kr.it.pullit.modules.questionset.repository.QuestionSetRepository;
+import kr.it.pullit.modules.questionset.web.dto.QuestionSetDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class QuestionSetService implements QuestionSetPublicApi {}
+@Service
+public class QuestionSetService implements QuestionSetPublicApi {
+
+  private final QuestionSetRepository questionSetRepository;
+
+  @Autowired
+  public QuestionSetService(QuestionSetRepository questionSetRepository) {
+    this.questionSetRepository = questionSetRepository;
+  }
+
+  @Transactional(readOnly = true)
+  public QuestionSetDto questionSetGetById(Long id) {
+    QuestionSet questionSet =
+        questionSetRepository
+            .findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("문제집을 찾을 수 없습니다"));
+    return new QuestionSetDto(questionSet);
+  }
+}
