f46a1449aba4f00bf0a325290b05d280de53181f
jihwan-hong <124516776+flareseek@users.noreply.github.com>
2025-09-12 22:10:09 +0900

    btsk-32: SSE 연결 구현 (#51)
    * feat: sse 연결 구현
    
    * refactor: 메서드명 변경 (delete -> deleteById)
    
    * refactor: 메서드명 변경 (sendToClient -> sendToMember)
    
    * fix: style
    
    * fix: Mapping 문법 수정

---
+++ src/main/java/kr/it/pullit/modules/notification/repository/EmitterRepositoryImpl.java
@@ -0,0 +1,24 @@
+package kr.it.pullit.modules.notification.repository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public class EmitterRepositoryImpl implements EmitterRepository {
+  private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+  public void save(Long userId, SseEmitter emitter) {
+    emitters.put(userId, emitter);
+  }
+
+  public void deleteById(Long userId) {
+    emitters.remove(userId);
+  }
+
+  public Optional<SseEmitter> findById(Long userId) {
+    return Optional.ofNullable(emitters.get(userId));
+  }
+}
