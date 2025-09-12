54ecbd70df72d3e47331e397c3a81b16fc3c37de
jihwan-hong <124516776+flareseek@users.noreply.github.com>
2025-09-12 22:10:09 +0900

    btsk-32: SSE 연결 구현 (#51)
    * feat: sse 연결 구현
    
    * refactor: 메서드명 변경 (delete -> deleteById)
    
    * refactor: 메서드명 변경 (sendToClient -> sendToMember)
    
    * fix: style
    
    * fix: Mapping 문법 수정

---
+++ src/main/java/kr/it/pullit/modules/notification/service/NotificationService.java
@@ -0,0 +1,42 @@
+package kr.it.pullit.modules.notification.service;
+
+import java.io.IOException;
+import kr.it.pullit.modules.notification.repository.EmitterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+  private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 30; // 30분
+  private static final String EVENT_NAME = "notification";
+  private final EmitterRepository emitterRepository;
+
+  public SseEmitter subscribe(Long userId) {
+    SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
+    emitterRepository.save(userId, emitter);
+
+    emitter.onCompletion(() -> emitterRepository.deleteById(userId));
+    emitter.onTimeout(() -> emitterRepository.deleteById(userId));
+    emitter.onError((e) -> emitterRepository.deleteById(userId));
+
+    sendToMember(userId, "EventStream Created. userId: " + userId);
+
+    return emitter;
+  }
+
+  public void sendToMember(Long userId, Object data) {
+    emitterRepository
+        .findById(userId)
+        .ifPresent(
+            emitter -> {
+              try {
+                emitter.send(SseEmitter.event().name(EVENT_NAME).data(data));
+              } catch (IOException e) {
+                emitterRepository.deleteById(userId);
+                throw new RuntimeException(e);
+              }
+            });
+  }
+}
