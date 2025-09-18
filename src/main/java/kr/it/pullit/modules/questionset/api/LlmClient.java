3981e5eeea650086fe2d8a43090123845dc1bf9e
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-09-18 20:08:06 +0900

    btsk-48: timeout setting and AI 문제 생성 로직 리팩토링
    - `LlmClient.getPrompt` 메서드 개선: 문제 개수 policy setup
    - Spring MVC Async 설정 추가: tcp 요청 처리 타임아웃 시간 30분으로 설정.
    
    Signed-off-by: Hyeonjun0527 <osumaniaddict527@gmail.com>

--- src/main/java/kr/it/pullit/modules/questionset/api/LlmClient.java
+++ src/main/java/kr/it/pullit/modules/questionset/api/LlmClient.java
@@ -6,36 +6,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface LlmClient {
-  static String getPrompt(
-      String difficultyPrompt, String questionTypePrompt, String examplePrompt) {
-    return String.format(
-        """
-            당신은 해당 pdf를 기반으로하는 시험 문제 출제 위원입니다.
-            따라서 당신은 수험자들의 학습을 잘 했는지 확인 할 수 있게 중요한 개념과 학생들이 어려워 하는 부분들을 문제로 출제하세요.
+  static String getPrompt(String difficultyPrompt, String questionTypePrompt, String examplePrompt,
+      int questionCount) {
+    return String.format("""
+        당신은 해당 pdf를 기반으로하는 시험 문제 출제 위원입니다.
+        따라서 당신은 수험자들의 학습을 잘 했는지 확인 할 수 있게 중요한 개념과 학생들이 어려워 하는 부분들을 문제로 출제하세요.
 
-            [지시사항]:
-            - Think step by step
-            - 문제유형: %s
-            - 난이도: %s
-            - 오답은 모호하면 안됩니다. 확실하게 오답이어야 합니다.
-            - 학생들이 어려워 하거나, 중요하다고 생각되는 부분을 문제로 출제해야 합니다.
-            - 자기검증: 최종 정보에 대해서, 스스로 검증한 후 대답해야 합니다.
-            - 모르는 정보는 절대 문제로 출제해서는 안됩니다.
-            - 각각 다른 문제를 생성하세요.
-            - 아래 예시를 참조하여 만드세요.
+        [지시사항]:
+        - Think step by step
+        - 문제유형: %s
+        - 난이도: %s
+        - 문제 개수: %d개
+        - 오답은 모호하면 안됩니다. 확실하게 오답이어야 합니다.
+        - 학생들이 어려워 하거나, 중요하다고 생각되는 부분을 문제로 출제해야 합니다.
+        - 자기검증: 최종 정보에 대해서, 스스로 검증한 후 대답해야 합니다.
+        - 모르는 정보는 절대 문제로 출제해서는 안됩니다.
+        - 각각 다른 문제를 생성하세요.
+        - 아래 예시를 참조하여 만드세요.
 
-            %s
-            """,
-        questionTypePrompt, difficultyPrompt, examplePrompt);
+        %s
+        """, questionTypePrompt, difficultyPrompt, questionCount, examplePrompt);
   }
 
-  List<LlmGeneratedQuestionDto> getLlmGeneratedQuestionContent(
-      String prompt, List<byte[]> fileData, int questionCount, String model);
+  List<LlmGeneratedQuestionDto> getLlmGeneratedQuestionContent(String prompt, List<byte[]> fileData,
+      int questionCount, String model);
 
-  void getLlmGeneratedQuestionStream(
-      String prompt,
-      List<byte[]> fileData,
-      int questionCount,
-      String model,
-      SseDataCallback callback);
+  void getLlmGeneratedQuestionStream(String prompt, List<byte[]> fileData, int questionCount,
+      String model, SseDataCallback callback);
 }
