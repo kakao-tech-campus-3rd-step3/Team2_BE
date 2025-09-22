c71e046406e714cc594213ae4e80c127c7e7bd0d
flareseek <flareseek@gmail.com>
2025-09-22 23:01:06 +0900

    style: fix style

--- src/main/java/kr/it/pullit/modules/questionset/domain/entity/Question.java
+++ src/main/java/kr/it/pullit/modules/questionset/domain/entity/Question.java
@@ -1,6 +1,15 @@
 package kr.it.pullit.modules.questionset.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import java.util.List;
 import kr.it.pullit.shared.jpa.BaseEntity;
 import lombok.Getter;
