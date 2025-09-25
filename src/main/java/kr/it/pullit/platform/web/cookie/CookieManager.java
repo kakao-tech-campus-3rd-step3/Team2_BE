d22821b13709bb11b571fbd8e5cae5669a4279c2
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-09-25 18:24:16 +0900

    hotfix: 코드 스타일수정

--- src/main/java/kr/it/pullit/platform/web/cookie/CookieManager.java
+++ src/main/java/kr/it/pullit/platform/web/cookie/CookieManager.java
@@ -1,12 +1,12 @@
 package kr.it.pullit.platform.web.cookie;
 
-import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.it.pullit.platform.security.jwt.JwtProps;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
@@ -17,8 +17,8 @@ public class CookieManager {
 
   private final JwtProps jwtProps;
 
-  public void addRefreshTokenCookie(HttpServletRequest request, HttpServletResponse response,
-      String refreshToken) {
+  public void addRefreshTokenCookie(
+      HttpServletRequest request, HttpServletResponse response, String refreshToken) {
     long maxAge = jwtProps.refreshTokenExpirationDays().getSeconds();
     ResponseCookie cookie = createRefreshTokenCookie(request, refreshToken, maxAge);
     log.info("Generated Refresh Token Cookie string: {}", cookie.toString());
@@ -30,11 +30,15 @@ public class CookieManager {
     response.addHeader("Set-Cookie", cookie.toString());
   }
 
-  private ResponseCookie createRefreshTokenCookie(HttpServletRequest request, String value,
-      long maxAge) {
-    ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie
-        .from(REFRESH_TOKEN_COOKIE_NAME, value).httpOnly(true).secure(request.isSecure()).path("/")
-        .maxAge(maxAge).sameSite(request.isSecure() ? "None" : "Lax");
+  private ResponseCookie createRefreshTokenCookie(
+      HttpServletRequest request, String value, long maxAge) {
+    ResponseCookie.ResponseCookieBuilder cookieBuilder =
+        ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, value)
+            .httpOnly(true)
+            .secure(request.isSecure())
+            .path("/")
+            .maxAge(maxAge)
+            .sameSite(request.isSecure() ? "None" : "Lax");
 
     if (jwtProps.cookieDomain() != null && !jwtProps.cookieDomain().isBlank()) {
       cookieBuilder.domain(jwtProps.cookieDomain());
