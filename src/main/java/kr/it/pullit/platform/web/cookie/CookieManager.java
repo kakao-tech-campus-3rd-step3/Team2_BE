2fb63279ad0b6f0b557c980b82ac82bc30ca34b8
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-09-25 18:21:31 +0900

    hotfix: 쿠키를 누락했음. getHeader->getHeaders

--- src/main/java/kr/it/pullit/platform/web/cookie/CookieManager.java
+++ src/main/java/kr/it/pullit/platform/web/cookie/CookieManager.java
@@ -1,12 +1,14 @@
 package kr.it.pullit.platform.web.cookie;
 
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.it.pullit.platform.security.jwt.JwtProps;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CookieManager {
@@ -15,10 +17,11 @@ public class CookieManager {
 
   private final JwtProps jwtProps;
 
-  public void addRefreshTokenCookie(
-      HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+  public void addRefreshTokenCookie(HttpServletRequest request, HttpServletResponse response,
+      String refreshToken) {
     long maxAge = jwtProps.refreshTokenExpirationDays().getSeconds();
     ResponseCookie cookie = createRefreshTokenCookie(request, refreshToken, maxAge);
+    log.info("Generated Refresh Token Cookie string: {}", cookie.toString());
     response.addHeader("Set-Cookie", cookie.toString());
   }
 
@@ -27,15 +30,11 @@ public class CookieManager {
     response.addHeader("Set-Cookie", cookie.toString());
   }
 
-  private ResponseCookie createRefreshTokenCookie(
-      HttpServletRequest request, String value, long maxAge) {
-    ResponseCookie.ResponseCookieBuilder cookieBuilder =
-        ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, value)
-            .httpOnly(true)
-            .secure(request.isSecure())
-            .path("/")
-            .maxAge(maxAge)
-            .sameSite(request.isSecure() ? "None" : "Lax");
+  private ResponseCookie createRefreshTokenCookie(HttpServletRequest request, String value,
+      long maxAge) {
+    ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie
+        .from(REFRESH_TOKEN_COOKIE_NAME, value).httpOnly(true).secure(request.isSecure()).path("/")
+        .maxAge(maxAge).sameSite(request.isSecure() ? "None" : "Lax");
 
     if (jwtProps.cookieDomain() != null && !jwtProps.cookieDomain().isBlank()) {
       cookieBuilder.domain(jwtProps.cookieDomain());
