33d1c089f3bfecb783ca6c785400433e56435091
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-09-29 18:44:33 +0900

    btsk-00: sentry 도입

--- src/main/java/kr/it/pullit/modules/auth/web/AuthController.java
+++ src/main/java/kr/it/pullit/modules/auth/web/AuthController.java
@@ -1,5 +1,6 @@
 package kr.it.pullit.modules.auth.web;
 
+import io.sentry.Sentry;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.it.pullit.modules.auth.service.AuthService;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,16 @@ public class AuthController {
     authService.logout(memberId);
     cookieManager.removeRefreshTokenCookie(request, response);
     return ResponseEntity.noContent().build();
+  }
+  @GetMapping("/test")
+  public ResponseEntity<String> test() {
+
+    try {
+      throw new Exception("This is a test.");
+    } catch (Exception e) {
+      Sentry.captureException(e);
+    }
+
+    return ResponseEntity.ok("test");
   }
 }
