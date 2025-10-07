23788f8df5bd4d4039f9751e2dd1c2e3338483f4
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-10-07 19:27:08 +0900

    btsk-72: SRP 준수를 위해 로직 리팩토링 Member 도메인에 제약 추가 구체적인 emitter를 서비스가 모르게 하고,NotificationChannel이 emitter를 감쌈 NotificationChannel은 스스로 채널 생성과 이벤트 전송 방식을 결정함. SSE 연결의 사후 처리를 notificationService가 직접 하고 있었던 것을 이벤트 분리함.
    Signed-off-by: Hyeonjun0527 <osumaniaddict527@gmail.com>

--- src/main/java/kr/it/pullit/modules/notification/domain/EventData.java
+++ src/main/java/kr/it/pullit/modules/notification/domain/EventData.java
@@ -1,3 +1,10 @@
 package kr.it.pullit.modules.notification.domain;
 
-public record EventData(long id, String name, Object data) {}
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public record EventData(long id, String name, Object data) {
+
+  public SseEmitter.SseEventBuilder toSseEventBuilder() {
+    return SseEmitter.event().id(String.valueOf(id)).name(name).data(data);
+  }
+}
