910d8676901c0c9c0f32758ba5f95bbb3d7a7bb1
최현준 <osumaniaddict527@gmail.com>
2025-10-10 18:13:05 +0900

    btsk-72: question set service에 SRE 원칙 적용 (#96)
    * btsk-72: SRP 준수를 위해 로직 리팩토링
    Member 도메인에 제약 추가
    구체적인 emitter를 서비스가 모르게 하고,NotificationChannel이 emitter를 감쌈
    NotificationChannel은 스스로 채널 생성과 이벤트 전송 방식을 결정함.
    SSE 연결의 사후 처리를 notificationService가 직접 하고 있었던 것을 이벤트 분리함.
    
    Signed-off-by: Hyeonjun0527 <osumaniaddict527@gmail.com>
    
    * refactor: SRP 단일책임원칙 반영
    
    * btsk-72: 코드 스타일 수정
    
    * btsk-72:questionService SRE 원칙 적용
    
    * btsk-72:question service SRE 적용
    
    * btsk-72:quesiton-set-service와 marking-service 리팩토링 SRE
    
    * btsk-72: 코드스타일 적용
    
    ---------
    
    Signed-off-by: Hyeonjun0527 <osumaniaddict527@gmail.com>

--- src/main/java/kr/it/pullit/modules/questionset/domain/event/QuestionSetCreatedEvent.java
+++ src/main/java/kr/it/pullit/modules/questionset/domain/event/QuestionSetCreatedEvent.java
@@ -1,3 +1,10 @@
 package kr.it.pullit.modules.questionset.domain.event;
 
-public record QuestionSetCreatedEvent(Long questionSetId, Long ownerId) {}
+import kr.it.pullit.modules.questionset.domain.entity.QuestionSet;
+
+public record QuestionSetCreatedEvent(Long questionSetId, Long ownerId) {
+
+  public static QuestionSetCreatedEvent from(QuestionSet questionSet) {
+    return new QuestionSetCreatedEvent(questionSet.getId(), questionSet.getOwner().getId());
+  }
+}
--- src/main/java/kr/it/pullit/modules/questionset/service/QuestionSetService.java
+++ src/main/java/kr/it/pullit/modules/questionset/service/QuestionSetService.java
@@ -1,13 +1,9 @@
 package kr.it.pullit.modules.questionset.service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import kr.it.pullit.modules.learningsource.source.api.SourcePublicApi;
 import kr.it.pullit.modules.learningsource.source.domain.entity.Source;
-import kr.it.pullit.modules.learningsource.source.exception.SourceNotFoundException;
 import kr.it.pullit.modules.member.api.MemberPublicApi;
 import kr.it.pullit.modules.member.domain.entity.Member;
 import kr.it.pullit.modules.member.exception.MemberNotFoundException;
@@ -62,26 +58,27 @@ public class QuestionSetService implements QuestionSetPublicApi {
             .findByIdWithQuestionsForFirstSolving(id, memberId)
             .orElseThrow(() -> handleQuestionSetNotFound(id, memberId));
 
-    return new QuestionSetResponse(questionSet);
+    return QuestionSetResponse.from(questionSet);
   }
 
   private QuestionSetResponse getQuestionSetForReviewing(Long id, Long memberId) {
     return questionSetRepository
         .findQuestionSetForReviewing(id, memberId)
-        .map(QuestionSetResponse::new)
-        .orElseThrow(
-            () -> {
-              QuestionSet qs =
-                  questionSetRepository
-                      .findByIdAndMemberId(id, memberId)
-                      .orElseThrow(() -> QuestionSetNotFoundException.byId(id));
+        .map(QuestionSetResponse::from)
+        .orElseThrow(() -> handleReviewSetNotFound(id, memberId));
+  }
 
-              if (qs.getStatus() != QuestionSetStatus.COMPLETE) {
-                return handleQuestionSetStatusException(qs);
-              }
+  private RuntimeException handleReviewSetNotFound(Long id, Long memberId) {
+    QuestionSet qs =
+        questionSetRepository
+            .findByIdAndMemberId(id, memberId)
+            .orElseThrow(() -> QuestionSetNotFoundException.byId(id));
 
-              return WrongAnswerNotFoundException.withMessage("복습할 오답이 없습니다.");
-            });
+    if (qs.getStatus() != QuestionSetStatus.COMPLETE) {
+      return handleQuestionSetStatusException(qs);
+    }
+
+    return WrongAnswerNotFoundException.withMessage("복습할 오답이 없습니다.");
   }
 
   private RuntimeException handleQuestionSetNotFound(Long id, Long memberId) {
@@ -101,75 +98,38 @@ public class QuestionSetService implements QuestionSetPublicApi {
 
   @Transactional
   public QuestionSetResponse create(QuestionSetCreateRequestDto request, Long ownerId) {
-    List<Source> sources = sourcePublicApi.findByIdIn(request.sourceIds());
-
-    if (sources.isEmpty()) {
-      throw SourceNotFoundException.withMessage("소스가 존재하지 않습니다.");
-    }
-
     Member owner =
         memberPublicApi.findById(ownerId).orElseThrow(() -> MemberNotFoundException.byId(ownerId));
+    List<Source> sources = sourcePublicApi.findByIdIn(request.sourceIds());
 
-    Set<Source> sourceSet = new HashSet<>(sources);
-    String title = sources.getFirst().getOriginalName();
-
-    QuestionSet questionSet =
-        new QuestionSet(
-            owner, sourceSet, title, request.difficulty(), request.type(), request.questionCount());
+    QuestionSet questionSet = QuestionSet.create(owner, sources, request);
 
     QuestionSet savedQuestionSet = questionSetRepository.save(questionSet);
 
-    eventPublisher.publishEvent(new QuestionSetCreatedEvent(savedQuestionSet.getId(), ownerId));
+    eventPublisher.publishEvent(QuestionSetCreatedEvent.from(savedQuestionSet));
 
-    return new QuestionSetResponse(savedQuestionSet);
+    return QuestionSetResponse.from(savedQuestionSet);
   }
 
   @Override
   @Transactional(readOnly = true)
   public List<MyQuestionSetsResponse> getMemberQuestionSets(Long memberId) {
     memberPublicApi.findById(memberId).orElseThrow(() -> MemberNotFoundException.byId(memberId));
-
     List<QuestionSet> questionSets = questionSetRepository.findByMemberId(memberId);
-    List<MyQuestionSetsResponse> myQuestionSetsResponses = new ArrayList<>();
-
-    for (QuestionSet questionSet : questionSets) {
-      List<Long> sourceIds = questionSet.getSources().stream().map(Source::getId).toList();
-      List<String> sourceNames =
-          questionSet.getSources().stream().map(Source::getOriginalName).toList();
-      MyQuestionSetsResponse response =
-          MyQuestionSetsResponse.builder()
-              .questionSetId(questionSet.getId())
-              .title(questionSet.getTitle())
-              .sourceIds(sourceIds)
-              .sourceNames(sourceNames)
-              .questionCount(questionSet.getQuestionLength())
-              .difficultyType(questionSet.getDifficulty())
-              .questionType(questionSet.getType())
-              .createdAt(questionSet.getCreatedAt())
-              .build();
-      myQuestionSetsResponses.add(response);
-    }
-
-    return myQuestionSetsResponses;
+    return questionSets.stream().map(MyQuestionSetsResponse::from).toList();
   }
 
   @Override
   @Transactional
   public void markAsComplete(Long questionSetId) {
-    QuestionSet questionSet =
-        questionSetRepository
-            .findById(questionSetId)
-            .orElseThrow(() -> QuestionSetNotFoundException.byId(questionSetId));
+    QuestionSet questionSet = findQuestionSetOrThrow(questionSetId);
     questionSet.completeProcessing();
   }
 
   @Override
   @Transactional
   public void markAsFailed(Long questionSetId) {
-    QuestionSet questionSet =
-        questionSetRepository
-            .findById(questionSetId)
-            .orElseThrow(() -> QuestionSetNotFoundException.byId(questionSetId));
+    QuestionSet questionSet = findQuestionSetOrThrow(questionSetId);
     questionSet.failProcessing();
   }
 
@@ -207,5 +167,18 @@ public class QuestionSetService implements QuestionSetPublicApi {
   @Transactional(readOnly = true)
   public Optional<QuestionSet> findEntityByIdAndMemberId(Long id, Long memberId) {
     return questionSetRepository.findByIdWithoutQuestions(id, memberId);
+  }
+
+  @Override
+  @Transactional
+  public void updateTitle(Long questionSetId, String title) {
+    QuestionSet questionSet = findQuestionSetOrThrow(questionSetId);
+    questionSet.updateTitle(title);
+  }
+
+  private QuestionSet findQuestionSetOrThrow(Long questionSetId) {
+    return questionSetRepository
+        .findById(questionSetId)
+        .orElseThrow(() -> QuestionSetNotFoundException.byId(questionSetId));
   }
 }
