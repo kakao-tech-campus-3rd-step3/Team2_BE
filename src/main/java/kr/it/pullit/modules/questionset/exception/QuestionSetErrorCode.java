23a56c987cd16263cf7c824f2ea99d81c9567ac5
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-10-19 14:01:43 +0900

    btsk-87: 문제집관련 에러 수정 및 securityconfig notificationservice 로그수준변경

--- src/main/java/kr/it/pullit/modules/questionset/exception/QuestionSetErrorCode.java
+++ src/main/java/kr/it/pullit/modules/questionset/exception/QuestionSetErrorCode.java
@@ -13,7 +13,8 @@ public enum QuestionSetErrorCode implements ErrorCode {
   QUESTION_SET_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "QSE_003", "문제집 생성에 실패했습니다."),
   WRONG_ANSWER_NOT_FOUND(HttpStatus.NOT_FOUND, "QSE_004", "복습할 오답이 없습니다."),
   QUESTION_SET_NOT_COMPLETE(HttpStatus.BAD_REQUEST, "QSE_005", "문제집이 완료되지 않았습니다."),
-  QUESTION_SET_UNAUTHORIZED(HttpStatus.FORBIDDEN, "QSE_006", "해당 문제집에 대한 권한이 없습니다.");
+  QUESTION_SET_UNAUTHORIZED(HttpStatus.FORBIDDEN, "QSE_006", "해당 문제집에 대한 권한이 없습니다."),
+  SOURCE_NOT_READY(HttpStatus.BAD_REQUEST, "QSE_007", "아직 처리 중인 소스 파일이라 문제집을 생성할 수 없습니다.");
 
   private final HttpStatus status;
   private final String code;
