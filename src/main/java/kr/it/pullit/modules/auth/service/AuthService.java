d13bf337c4ef6219a38fbd19753a1afb9c67a82f
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-10-20 21:28:28 +0900

    hotfix: 로그인 안되는 문제 디버깅

--- src/main/java/kr/it/pullit/modules/auth/service/AuthService.java
+++ src/main/java/kr/it/pullit/modules/auth/service/AuthService.java
@@ -8,14 +8,14 @@ import kr.it.pullit.platform.security.jwt.JwtTokenProvider;
 import kr.it.pullit.platform.security.jwt.dto.AuthTokens;
 import kr.it.pullit.platform.security.jwt.dto.TokenCreationSubject;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-
-  // TODO: REVIEW-3 : 메서드를 기능 단위로 추출해보세요. 그리고 값을 꺼내서 처리하고 있다면 Value Object를 도출하거나 객체에게 메시지를 보내보세요.
 
   private final MemberPublicApi memberPublicApi;
   private final JwtTokenProvider jwtTokenPort;
@@ -27,23 +27,17 @@ public class AuthService {
             .findById(memberId)
             .orElseThrow(() -> MemberNotFoundException.byId(memberId));
 
-    String existingRefreshToken = member.getRefreshToken();
-
-    // 기존 리프레시 토큰이 있고, 유효하다면 재사용
-    if (jwtTokenPort.validateToken(existingRefreshToken).isValid()) {
-      String newAccessToken = jwtTokenPort.createAccessToken(TokenCreationSubject.from(member));
-      return new AuthTokens(newAccessToken, existingRefreshToken);
-    }
-
-    // 기존 토큰이 없거나 유효하지 않으면 새로 발급
     AuthTokens newAuthTokens = jwtTokenPort.createAuthTokens(TokenCreationSubject.from(member));
     member.updateRefreshToken(newAuthTokens.refreshToken());
+
+    log.info(" [토큰 발급] DB에 저장된 리프레시 토큰: {}", newAuthTokens.refreshToken());
 
     return newAuthTokens;
   }
 
   @Transactional(readOnly = true)
   public String reissueAccessToken(String refreshToken) {
+    log.info(" [토큰 갱신] API로 전달받은 리프레시 토큰: {}", refreshToken);
     validateRefreshToken(refreshToken);
 
     Member member =
