c35020e0dfdac6a793d4c7765869b4ff81e1ff76
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-10-28 01:40:29 +0900

    btsk-90: 충돌안나도록 리팩토링

--- src/test/java/kr/it/pullit/support/fixture/QuestionSetFixtures2.java
+++ src/test/java/kr/it/pullit/support/fixture/QuestionSetFixtures2.java
@@ -3,21 +3,21 @@ package kr.it.pullit.support.fixture;
 import java.util.List;
 import kr.it.pullit.modules.questionset.domain.entity.Question;
 import kr.it.pullit.modules.questionset.domain.entity.QuestionSet;
-import kr.it.pullit.support.builder.TestQuestionSetBuilder;
+import kr.it.pullit.support.builder.TestQuestionSetBuilder2;
 
-public final class QuestionSetFixtures {
+public final class QuestionSetFixtures2 {
 
-  private QuestionSetFixtures() {}
+  private QuestionSetFixtures2() {}
 
   public static QuestionSet basic() {
-    return TestQuestionSetBuilder.builder().build();
+    return TestQuestionSetBuilder2.builder().build();
   }
 
   public static QuestionSet withOwner(Long ownerId) {
-    return TestQuestionSetBuilder.builder().ownerId(ownerId).build();
+    return TestQuestionSetBuilder2.builder().ownerId(ownerId).build();
   }
 
   public static QuestionSet withQuestions(List<Question> questions) {
-    return TestQuestionSetBuilder.builder().questions(questions).build();
+    return TestQuestionSetBuilder2.builder().questions(questions).build();
   }
 }
