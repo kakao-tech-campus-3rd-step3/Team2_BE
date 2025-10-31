7204deb57ce0083521029fa1132e2e5cf364b9b7
mouiye <2jihye10@gmail.com>
2025-10-31 16:23:36 +0900

    refactor: 잘못된 api 경로 수정

--- src/main/java/kr/it/pullit/modules/wronganswer/web/apidocs/WrongAnswerApiDocs.java
+++ src/main/java/kr/it/pullit/modules/wronganswer/web/apidocs/WrongAnswerApiDocs.java
@@ -5,27 +5,26 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import kr.it.pullit.shared.apidocs.ApiDocsGroup;
-import org.springframework.http.ProblemDetail;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import kr.it.pullit.shared.apidocs.ApiDocsGroup;
+import org.springframework.http.ProblemDetail;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @ApiDocsGroup
 @ApiResponses(
     value = {
-        @ApiResponse(
-            responseCode = "404",
-            description = "요청한 오답 데이터를 찾을 수 없음",
-            content =
-            @Content(
-                mediaType = "application/json",
-                schema = @Schema(implementation = ProblemDetail.class),
-                examples = {
+      @ApiResponse(
+          responseCode = "404",
+          description = "요청한 오답 데이터를 찾을 수 없음",
+          content =
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProblemDetail.class),
+                  examples = {
                     @ExampleObject(
                         name = "WRONG_ANSWER_NOT_FOUND",
                         summary = "오답 문제를 찾을 수 없음 (WAE_001)",
@@ -36,7 +35,7 @@ import java.lang.annotation.Target;
                                   "title": "Not Found",
                                   "status": 404,
                                   "detail": "오답을 찾을 수 없습니다. (Member ID, Question ID: 91, 100)",
-                                  "instance": "/api/wronganswers",
+                                  "instance": "/api/wrong-answers",
                                   "code": "WAE_001"
                                 }
                                 """),
@@ -50,11 +49,10 @@ import java.lang.annotation.Target;
                                   "title": "Not Found",
                                   "status": 404,
                                   "detail": "복습할 오답이 없습니다.",
-                                  "instance": "/api/wrong-anwsers",
+                                  "instance": "/api/wrong-answers",
                                   "code": "WAN_002"
                                 }
                                 """)
-                }))
+                  }))
     })
-public @interface WrongAnswerApiDocs {
-}
+public @interface WrongAnswerApiDocs {}
