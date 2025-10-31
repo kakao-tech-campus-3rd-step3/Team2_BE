69a648778f9d42d792daf98764b432b19b371c2c
JihyeLee <126856188+xqqldir@users.noreply.github.com>
2025-11-01 02:44:32 +0900

    btsk-92: source tescode 및 api docs (#117)
    * feat: source testcode and apidocs
    
    * refactor: checkstyle 수정
    
    * refactor
    
    * refactor
    
    * refactor: sourcePublicApi 수정
    
    SourcePublicApi의 findByIdAndMemberId에서 id만 전달하던 것을 id와 member id를 전달하게 함으로써 보안을 강화함
    
    * refactor: checkstyle
    
    * btsk-94: 코드리뷰 반영 todo 제작
    
    ---------
    
    Co-authored-by: Hyeonjun0527 <osumaniaddict527@gmail.com>

---
+++ src/test/java/kr/it/pullit/modules/learningsource/source/web/dto/SourceResponseTest.java
@@ -0,0 +1,43 @@
+package kr.it.pullit.modules.learningsource.source.web.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kr.it.pullit.modules.learningsource.source.domain.entity.Source;
+import kr.it.pullit.modules.learningsource.source.domain.entity.SourceCreationParam;
+import kr.it.pullit.modules.learningsource.sourcefolder.domain.entity.SourceFolder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SourceResponseTest {
+
+  @Test
+  @DisplayName("폴더 정보가 없으면 응답의 폴더명이 null이다")
+  void fromReturnsNullFolderNameWhenSourceHasNoFolder() {
+    Source source =
+        Source.create(
+            new SourceCreationParam(
+                1L, "sample.pdf", "learning-sources/sample.pdf", "application/pdf", 2048L),
+            1L,
+            null);
+
+    SourceResponse response = SourceResponse.from(source);
+
+    assertThat(response.sourceFolderName()).isNull();
+  }
+
+  @Test
+  @DisplayName("폴더가 있으면 응답에 폴더명이 채워진다")
+  void fromReturnsFolderNameWhenSourceHasFolder() {
+    SourceFolder folder = SourceFolder.createDefaultFolder(2L);
+    Source source =
+        Source.create(
+            new SourceCreationParam(
+                2L, "other.pdf", "learning-sources/other.pdf", "application/pdf", 1024L),
+            2L,
+            folder);
+
+    SourceResponse response = SourceResponse.from(source);
+
+    assertThat(response.sourceFolderName()).isEqualTo(folder.getName());
+  }
+}
