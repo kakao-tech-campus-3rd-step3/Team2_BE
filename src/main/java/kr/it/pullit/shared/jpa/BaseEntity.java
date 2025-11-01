887671407e08488f0e1223af5c174b9729f05d7f
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-11-01 17:56:29 +0900

    hotfix: 재적용

--- src/main/java/kr/it/pullit/shared/jpa/BaseEntity.java
+++ src/main/java/kr/it/pullit/shared/jpa/BaseEntity.java
@@ -1,15 +1,15 @@
 package kr.it.pullit.shared.jpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @NoArgsConstructor // SuperBuilder는 생성자를 필요로 합니다.
@@ -21,6 +21,7 @@ public abstract class BaseEntity {
   @CreatedDate
   @Column(nullable = false, updatable = false)
   private LocalDateTime createdAt;
+  // 재적용
 
   @LastModifiedDate
   @Column(nullable = false)
