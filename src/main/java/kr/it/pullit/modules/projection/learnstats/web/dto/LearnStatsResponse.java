43819c161567e5b44b7f0c40d7c61a76d5aeee58
최현준 <osumaniaddict527@gmail.com>
2025-11-04 02:13:09 +0900

    btsk-106: 프메 그라파나 적용 (#134)
    * feat: 프메 그라파나 적용
    
    * refactor: 명칭변경
    
    * feat: learnstatresponse 수정
    
    * refactor: 이름 변경

--- src/main/java/kr/it/pullit/modules/projection/learnstats/web/dto/LearnStatsResponse.java
+++ src/main/java/kr/it/pullit/modules/projection/learnstats/web/dto/LearnStatsResponse.java
@@ -1,7 +1,7 @@
 package kr.it.pullit.modules.projection.learnstats.web.dto;
 
-import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDate;
+import jakarta.validation.constraints.PositiveOrZero;
 import kr.it.pullit.modules.projection.learnstats.domain.LearnStats;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,9 +19,10 @@ public class LearnStatsResponse {
   @PositiveOrZero private int totalSolvedQuestionSetCount; // 완료한 문제집 수
 
   @PositiveOrZero private long totalQuestionCount; // 전체 문제 수
-  @PositiveOrZero private long totalSolvedQuestionCount; // 총 푼 문제 수
+  @PositiveOrZero private long totalSolvedQuestionCount; // 총 시도한 문제 수
+  @PositiveOrZero private long totalCorrectQuestionCount; // 총 맞은 문제 수
 
-  @PositiveOrZero private int weeklySolvedQuestionCount; // 이번 주 푼 문제 수
+  @PositiveOrZero private int weeklySolvedQuestionCount; // 이번 주 시도한 문제 수
 
   @PositiveOrZero private int consecutiveLearningDays; // 연속 학습일
 
@@ -36,6 +37,7 @@ public class LearnStatsResponse {
         .totalSolvedQuestionSetCount(p.getTotalSolvedQuestionSetCount())
         .totalQuestionCount(p.getTotalQuestionCount())
         .totalSolvedQuestionCount(p.getTotalSolvedQuestionCount())
+        .totalCorrectQuestionCount(p.getTotalCorrectQuestionCount())
         .weeklySolvedQuestionCount(p.getWeeklySolvedQuestionCount())
         .consecutiveLearningDays(p.getConsecutiveLearningDays())
         .lastLearningDate(p.getLastLearningDate())
