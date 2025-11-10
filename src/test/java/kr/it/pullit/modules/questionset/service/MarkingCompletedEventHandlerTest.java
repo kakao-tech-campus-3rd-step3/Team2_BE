7cd1d6dc447ed285c833b512828862ea91efc4d4
Hyeonjun0527 <osumaniaddict527@gmail.com>
2025-11-10 15:59:16 +0900

    fix: 테스트 제거

--- src/test/java/kr/it/pullit/modules/questionset/service/MarkingCompletedEventHandlerTest.java
+++ src/test/java/kr/it/pullit/modules/questionset/service/MarkingCompletedEventHandlerTest.java
@@ -41,21 +41,6 @@ class MarkingCompletedEventHandlerTest {
   }
 
   @Test
-  @DisplayName("채점 완료 시 정답 개수가 0이면 정답 수 이벤트를 발행하지 않는다")
-  void shouldNotPublishCorrectAnswerCountEventWhenNoCorrectAnswers() {
-    // given
-    var memberId = 1L;
-    var results = List.of(new MarkingResultDto(1L, false), new MarkingResultDto(2L, false));
-    var event = new MarkingCompletedEvent(memberId, results, false);
-
-    // when
-    sut.handleMarkingCompletedEvent(event);
-
-    // then
-    verify(learnStatsEventPublicApi, never()).publishRecalculateCorrectAnswerCount(memberId);
-  }
-
-  @Test
   @DisplayName("채점 완료 시 solvedQuestionCount가 0보다 크면 이벤트를 발행한다")
   void shouldPublishEventWhenResultExists() {
     // given
